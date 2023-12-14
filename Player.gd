030437526d4428287aceba947cf8c06b05f99cdc
Travisbui <122054086+Travisbui@users.noreply.github.com>
2023-12-14 15:34:48 -0800

    commit #3

--- Player.gd
+++ Player.gd
@@ -3,10 +3,15 @@ extends Area2D
 @export var speed = 400
 
 func _process(delta):
-	velocity = Vector2.ZERO()
-	if Input.is_action_just_pressed("move_left")
+	var velocity = Vector2.ZERO
+	if Input.is_action_just_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_just_pressed("move_right")
+	if Input.is_action_just_pressed("move_right"):
 		velocity.x += 1
-	
-		
+	if Input.is_action_just_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_just_pressed("move_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
